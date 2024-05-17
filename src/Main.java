4f05c77ad74c4f840b693ffebff4aa065e85b517
Czuker224 <56686494+Czuker224@users.noreply.github.com>
2024-05-17 19:13:00 +0200

    Zmiany projektu zgodnie z założeniami zadania 3

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Tworzenie instancji klasy Worker
+        // Tworzenie instancji klasy Worker (dwie z tych samym id)
         Worker worker1 = new Worker("John Doe", 3000, 1, "2021-01-01", "Developer");
-        Worker worker2 = new Worker("Jane Smith", 3200, 2, "2020-02-01", "Tester");
-        Worker worker3 = new Worker("Alice Johnson", 3500, 3, "2019-03-01", "Designer");
-        Worker worker4 = new Worker("Bob Brown", 3300, 4, "2021-04-01", "Developer");
+        Worker worker2 = new Worker("Jane Smith", 3200, 1, "2020-02-01", "Tester");
+        Worker worker3 = new Worker("Alice Johnson", 3500, 2, "2019-03-01", "Designer");
 
         // Tworzenie instancji klasy Manager
-        Manager manager = new Manager("Emma Wilson", 5000, 5, "2018-05-01", "Manager");
+        Manager manager = new Manager("Emma Wilson", 5000, 3, "2018-05-01", "Manager");
 
-        // Tworzenie listy typu Employee
+        // Wyświetlanie hashCode dla wszystkich obiektów
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
+        System.out.println();
+
+        // Dodawanie obiektów do listy
         List<Employee> employees = new ArrayList<>();
         employees.add(worker1);
         employees.add(worker2);
         employees.add(worker3);
-        employees.add(worker4);
         employees.add(manager);
 
-        // Wyświetlanie danych i wywoływanie metody work dla każdego pracownika
+        // Iterowanie po liście i porównywanie metodą equals
         for (Employee employee : employees) {
-            employee.work();
-            System.out.println("-" + employee.getName() + " (ID: " + employee.hashCode() + ", Position: " + employee.getPosition() + ", Hire date: " + employee.getHireDate() + ", Salary: " + employee.getSalary() + ")");
+            System.out.println("worker2.equals(" + employee.getName() + "): " + worker2.equals(employee));
         }
     }
 }
